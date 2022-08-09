9af017cc9a4720cfd63d021d306b2638b1bec37a
José Manuel Gulias Lugo <chepe.time@gmail.com>
2022-08-08 21:58:42 -0500

    Fixed ui exports

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,3 @@
 import * as React from "react";
-export { Button, type ButtonProps } from "./Button";
-export { Example } from "./Example/Example";
+export { Button, type ButtonProps } from "./components/Button/Button";
+export { Example } from "./components/Example/Example";

1f039f54cb304a426f05a56ff2734922ed4730d3
José Manuel Gulias Lugo <chepe.time@gmail.com>
2022-08-10 11:35:54 -0500

    .gitignore is now working

--- packages/ui/dist/cjs/components/Switch/Switch.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-export interface SwitchProps {
-    children?: React.ReactNode;
-}
-export declare function Switch(props: SwitchProps): JSX.Element;
-export declare namespace Switch {
-    var displayName: string;
-}
-//# sourceMappingURL=Switch.d.ts.map
--- packages/ui/dist/esm/components/Paragraph/Paragraph.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-export interface ParagraphProps {
-    children?: React.ReactNode;
-}
-export declare function Paragraph(props: ParagraphProps): JSX.Element;
-export declare namespace Paragraph {
-    var displayName: string;
-}
-//# sourceMappingURL=Paragraph.d.ts.map

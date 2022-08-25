7f222bde9b3ab72e84c789f34bac4769cd32efbb
José Manuel Gulias Lugo <chepe.time@gmail.com>
2022-08-25 17:07:59 -0500

    Updated docs

--- packages/ui/src/components/Button/Button.test.tsx
+++ packages/ui/src/components/Button/Button.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { Button } from "./Button";
 
-describe("DexButton", () => {
+describe("Button", () => {
   test("renders the Button component", () => {
     render(<Button />);
   });

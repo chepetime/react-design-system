175186d211c8faf8ada5a368c616227b961db962
José Manuel Gulias Lugo <chepe.time@gmail.com>
2022-08-25 17:58:14 -0500

    Temp remove maps

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -1,11 +1,17 @@
+//This plugin prevents packages listed in peerDependencies from being bundled with our component library
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
+//efficiently bundles third party dependencies we've installed and use in node_modules
 import resolve from "@rollup/plugin-node-resolve";
+
+// //enables transpilation into CommonJS (CJS) format
 import commonjs from "@rollup/plugin-commonjs";
+
 import typescript from "@rollup/plugin-typescript";
 
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import dts from "rollup-plugin-dts";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import sourcemaps from "rollup-plugin-sourcemaps";
 
 import packageJson from "./package.json";
@@ -34,19 +40,23 @@ const rollupConfig = [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      sourcemaps(),
+      typescript({
+        tsconfig: "./tsconfig.json",
+        sourceMap: false,
+        inlineSources: false,
+      }),
       postcss({
         extract: false,
         modules: true,
         use: ["sass"],
       }),
+      sourcemaps(),
       terser(),
     ],
   },
   {
     input: "dist/esm/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    output: [{ file: "dist/index.d.ts", format: "esm", sourcemap: false }],
     plugins: [dts()],
     external: [/\.(css|scss)$/],
   },

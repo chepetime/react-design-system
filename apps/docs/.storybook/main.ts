fdbd11b9c8517f5994f64d2467bca58b8369285e
José Manuel Gulias Lugo <chepe.time@gmail.com>
2023-04-18 09:36:40 -0600

    Updated node deps

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,45 +1,33 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 
 import { mergeConfig } from "vite";
-
 const config = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-    "@storybook/addon-storysource",
-    "storybook-addon-designs",
-    "@storybook/preset-scss",
-    "@storybook/addon-coverage",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-storysource", "storybook-addon-designs", "@storybook/preset-scss", "@storybook/addon-coverage", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
   },
   features: {
     storyStoreV7: true,
     buildStoriesJson: true,
-    legacyDecoratorFileOrder: true,
+    legacyDecoratorFileOrder: true
   },
   async viteFinal(config) {
     return mergeConfig(config, {
       define: {
-        "process.env.NODE_DEBUG": false,
+        "process.env.NODE_DEBUG": false
       },
       resolve: {
-        alias: [
-          {
-            find: /^~(.*)$/,
-            replacement: "$1",
-          },
-        ],
-      },
+        alias: [{
+          find: /^~(.*)$/,
+          replacement: "$1"
+        }]
+      }
     });
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: "tag"
+  }
 };
 export default config;

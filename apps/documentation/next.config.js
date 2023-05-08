9aa275b067df652139139a2f6c443c783ece2557
José Manuel Gulias Lugo <chepe.time@gmail.com>
2023-05-08 16:48:05 -0600

    Correct configuration for documentation page

--- apps/documentation/next.config.js
+++ apps/documentation/next.config.js
@@ -1,7 +1,9 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  output: "export",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  output: "export",
+  images: { unoptimized: true },
+});

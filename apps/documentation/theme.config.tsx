43c3ec339e4239e79cdb74272ce7ed105ab14783
José Manuel Gulias Lugo <chepe.time@gmail.com>
2023-05-09 16:06:13 -0600

    Update documentation

--- apps/documentation/theme.config.tsx
+++ apps/documentation/theme.config.tsx
@@ -25,21 +25,24 @@ const config: DocsThemeConfig = {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="https://chepetime.github.io/react-design-system/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="https://chepetime.github.io/react-design-system/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="https://chepetime.github.io/react-design-system/favicon-16x16.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link
+        rel="manifest"
+        href="https://chepetime.github.io/react-design-system/site.webmanifest"
+      />
     </>
   ),
 };

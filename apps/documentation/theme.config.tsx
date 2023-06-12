8b52618597837ed089d5c3150089e5c869476eb0
José Manuel Gulias Lugo <chepe.time@gmail.com>
2023-06-12 13:44:52 -0600

    Add documentation structure

--- apps/documentation/theme.config.tsx
+++ apps/documentation/theme.config.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>React Desgn System</span>,
+  logo: <span>React Design System</span>,
   project: {
     link: "https://github.com/chepetime/react-design-system",
   },
